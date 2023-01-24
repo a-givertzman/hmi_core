131219748ab7613c5f3f7611d6f4e954f68e0e91
anton lobanov <a.givertzman@icloud.com>
2023-01-24 15:56:42 +0300

    test coverage

--- .github/workflows/coverage.sh
+++ .github/workflows/coverage.sh
@@ -54,6 +54,12 @@ done < "$path"
 # echo "results=$coverageResults" >> $GITHUB_OUTPUT
 if [[ !coverageExitStatus != 0 ]]; then 
     echo "#### 🔴 Some files are not enough covered by unit tests!" >> $GITHUB_STEP_SUMMARY
-    echo 'Please check details on your local machine using command: `flutter test --coverage`.' >> $GITHUB_STEP_SUMMARY
+    echo 'Please check details on your local machine using commands:'
+    echo '```'
+    echo '  flutter test --coverage' >> $GITHUB_STEP_SUMMARY
+    echo '  genhtml coverage/lcov.info -o coverage/html' >> $GITHUB_STEP_SUMMARY
+    echo '  open coverage/html/index.html'
+    echo '```'
+    # echo 'Please check details on your local machine using command: `flutter test --coverage`.' >> $GITHUB_STEP_SUMMARY
 fi
 exit $coverageExitStatus
