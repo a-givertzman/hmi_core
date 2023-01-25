5142ac71e171cda42b14810214ec5b68059f91b8
anton lobanov <a.givertzman@icloud.com>
2023-01-25 17:16:56 +0300

    DsDataType comment

--- lib/src/core/entities/ds_data_type.dart
+++ lib/src/core/entities/ds_data_type.dart
@@ -1,36 +1,30 @@
 import 'package:hmi_core/src/core/error/failure.dart';
 
 enum DsDataType {
-  /// Possible values for Siemens S7 controllers:
-  /// 
-  /// 0 or 1
+  /// Possible values: 
+  ///   - Siemens S7 controllers: 0 or 1
   bool('bool', 1),
-  /// Possible values for Siemens S7 controllers:
-  /// 
-  /// integers from -32768 to 32767
+  /// Possible values:
+  ///   - Siemens S7 controllers: integers from -32768 to 32767
   integer('int', 2),
-  /// Possible values for Siemens S7 controllers:
-  /// 
-  /// integers from 0 to 65535
+  /// Possible values:
+  ///   - Siemens S7 controllers: integers from 0 to 65535
   uInt('uint', 2),
-  /// Possible values for Siemens S7 controllers:
-  ///  
-  /// integers from -2147483648 to 2147483647
+  /// Possible values:
+  ///   - Siemens S7 controllers: integers from -2147483648 to 2147483647
   dInt('dint', 4),
-  /// Possible values for Siemens S7 controllers:
-  /// 
-  /// integers from 0 to 65535
+  /// Possible values:
+  ///   - Siemens S7 controllers: integers from 0 to 65535
   word('word', 2),
-  /// Possible values for Siemens S7 controllers:
-  /// 
-  /// integers from -9223372036854775808 to 9223372036854775807
+  /// Possible values:
+  ///   - Siemens S7 controllers: integers from -9223372036854775808 to 9223372036854775807
   lInt('lint', 8),
-  /// Possible values for Siemens S7 controllers:
-  ///   
-  ///   Integer depth: from 3.402823e+38 to -3.402823e+38
-  /// 
-  ///   Fractional depth: from -1.175495e-38 to 1.175495e-38
+  /// Possible values:
+  ///   - Siemens S7 controllers: 
+  ///     - int from 3.402823e+38 to -3.402823e+38
+  ///     - real from -1.175495e-38 to 1.175495e-38
   real('real', 4),
+  /// Possible values:
   time('time', 4),
   dateAndTime('date_and_time', 8);
   ///
@@ -59,3 +53,10 @@ enum DsDataType {
     return 'DsDataType {name: $name, length: $length}';
   }
 }
+final b = DsDataType.bool;
+final c = DsDataType.integer;
+final c1 = DsDataType.uInt;
+final c2 = DsDataType.dInt;
+final c3 = DsDataType.word;
+final c4 = DsDataType.lInt;
+final c5 = DsDataType.real;
