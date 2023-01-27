cc97c663d5074b6300871cfdcb3c1018cda0c81d
Minyewoo <const.trushov@gmail.com>
2023-01-27 16:06:07 +0300

    Implemented DsPointName

---
+++ lib/src/core/entities/ds_point_name.dart
@@ -0,0 +1,31 @@
+import 'package:hmi_core/hmi_core_failure.dart';
+///
+class DsPointName {
+  final String _fullPath;
+  final int _lastSeparatorIndex;
+  ///
+  DsPointName({required String fullPath}) : 
+    _fullPath = fullPath,
+    _lastSeparatorIndex = _findLastMatchIndex(fullPath, '/');
+  ///
+  String get path => _fullPath.substring(0, _lastSeparatorIndex+1);
+  ///
+  String get name => _fullPath.substring(_lastSeparatorIndex+1);
+  //
+  @override
+  String toString() => _fullPath;
+  ///
+  /// Finds index of last [symbol] appearance in [source] string.
+  static int _findLastMatchIndex(String source, String symbol) {
+    for(int i = source.length - 1; i >= 0; i--) {
+      if(source[i] == symbol) {
+        return i;
+      }
+    }
+    throw Failure(
+      message:
+        'Ошибка в методе $DsPointName._findLastSeparatorIndex: неверный формат пути: "$source"',
+      stackTrace: StackTrace.current,
+    );
+  }
+}
