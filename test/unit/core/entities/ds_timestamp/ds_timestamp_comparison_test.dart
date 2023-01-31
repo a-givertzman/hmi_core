b6816a5c882b40ae67aea2423d9498a4227643d8
anton lobanov <a.givertzman@icloud.com>
2023-01-31 13:53:21 +0300

    flutter analyser issues fized

--- test/unit/core/entities/ds_timestamp/ds_timestamp_comparison_test.dart
+++ test/unit/core/entities/ds_timestamp/ds_timestamp_comparison_test.dart
@@ -6,6 +6,7 @@ void main() {
     final datetime = DateTime.now();
     final timestamp = DsTimeStamp(dateTime: datetime);
     
+    // ignore: unrelated_type_equality_checks
     expect(timestamp == 'test', false);
     expect(timestamp == DsTimeStamp(dateTime: datetime), true);
     expect(timestamp == DsTimeStamp(dateTime: DateTime.now()), false);
