81881c704c3570ea1e10d309a0403188b14edcfc
Minyewoo <const.trushov@gmail.com>
2023-02-06 11:19:44 +0300

    Enhanced Localized.toString() test

--- test/unit/core/translate/localized_to_string_test.dart
+++ test/unit/core/translate/localized_to_string_test.dart
@@ -5,40 +5,60 @@ void main() {
   test('Localized toString', () async {
     final translationData = [
       {
-        'input': 'Crane monitoring',
-        'translation':'Диагностика крана',
+        'en_text': 'Crane monitoring',
+        'ru_text':'Диагностика крана',
       },
       {
-        'input': 'Harbour',
-        'translation':'В порту',
+        'en_text': 'Harbour',
+        'ru_text':'В порту',
       },
       {
-        'input': 'Winch 3',
-        'translation': 'Лебедка 3',
+        'en_text': 'Winch 3',
+        'ru_text': 'Лебедка 3',
       },
       {
-        'input': 'Knukle jib',
-        'translation': 'Хобот',
+        'en_text': 'Knukle jib',
+        'ru_text': 'Хобот',
       },
       {
-        'input': 'Basic protections',
-        'translation':'Основные защиты',
+        'en_text': 'Basic protections',
+        'ru_text':'Основные защиты',
       },
       {
-        'input': 'High pressure accumulator',
-        'translation':'Аккумулятор высокого давления',
+        'en_text': 'High pressure accumulator',
+        'ru_text':'Аккумулятор высокого давления',
       },
       {
-        'input': 'Text that has never been localized',
-        'translation': 'Text that has never been localized',
+        'en_text': 'Text that has never been localized',
+        'ru_text': 'Text that has never been localized',
+      },
+      {
+        'en_text': 'Непереведённый текст',
+        'ru_text': 'Непереведённый текст',
+      },
+      {
+        'en_text': '1234567890-=!"№;%:?*()_+',
+        'ru_text': '1234567890-=!"№;%:?*()_+',
       },
     ]; 
     for(final entry in translationData) {
-      final input = entry['input']!;
-      final translation = entry['translation']!;
-      expect(Localized(input).toString(), translation);
-      expect(Localized(input, lang: AppLang.ru).toString(), translation);
-      expect(Localized(input, lang: AppLang.en).toString(), input);
+      final englishText = entry['en_text']!;
+      final russianText = entry['ru_text']!;
+      expect(
+        Localized(englishText).toString(), 
+        russianText,
+        reason: 'Wrong translation to default language!'
+      );
+      expect(
+        Localized(englishText, lang: AppLang.ru).toString(), 
+        russianText,
+        reason: 'Wrong translation to russian language!'
+      );
+      expect(
+        Localized(englishText, lang: AppLang.en).toString(), 
+        englishText,
+        reason: 'Wrong translation to english language!'
+      );
     }
   });
 }
