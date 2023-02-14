08b93e9f186bc41c0b11ad17a6c24a872ec85727
anton lobanov <a.givertzman@icloud.com>
2023-02-14 16:13:03 +0300

    AppSettings with factor

--- lib/src/app_settings/setting.dart
+++ lib/src/app_settings/setting.dart
@@ -2,24 +2,31 @@ import 'app_settings.dart';
 
 class Setting {
   final String _name;
-  const Setting(String name) : _name = name;
+  final double _factor;
+  ///
+  const Setting(
+    String name, {
+    double factor = 1.0
+  }) : 
+    _name = name,
+    _factor = factor;
   /// 
   /// Returns setting value in int represantation
   int get toInt {
     final value = AppSettings.getSetting(_name);
     if (value is int) {
-      return value;
+      return _factor == 1 ? value : (value * _factor).toInt();
     }
-    return double.parse('$value').toInt();
+    return (double.parse('$value') * _factor).toInt();
   }
   /// 
   /// Returns setting value in double represantation
   double get toDouble {
     final value = AppSettings.getSetting(_name);
     if (value is double) {
-      return value;
+      return value * _factor;
     }
-    return double.parse('$value');
+    return double.parse('$value') * _factor;
   }
   /// 
   /// Returns setting value in string represantation
