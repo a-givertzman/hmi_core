b2093b6f6fe35f0cad20ab4bae8de52a9afd29ec
anton lobanov <a.givertzman@icloud.com>
2023-02-17 14:14:32 +0300

    Customisation-of-StreamFork

--- lib/src/core/stream/stream_fork.dart
+++ lib/src/core/stream/stream_fork.dart
@@ -46,3 +46,30 @@ class StreamFork<T> {
   /// Returns one of the forks of incoming stream.
   Stream<T> stream(int index) => _controllers[index].stream;
 }
+///
+/// Forks input stream on a single output stream.
+/// Output stream buffers incoming events before subscribtion occurs on it.
+class StreamForkSingle<T> extends StreamFork<T> {
+  /// 
+  /// Forks input [stream] on single output stream.
+  /// output stream buffers incoming events before subscribtion occurs on it.
+  StreamForkSingle(Stream<T> stream) : super(stream, listenersCount: 1);
+}
+///
+/// Forks input stream on 2 output streams.
+/// Each output stream buffers incoming events before subscribtion occurs on it.
+class StreamForkDouble<T> extends StreamFork<T> {
+  /// 
+  /// Forks input [stream] on 2 output streams.
+  /// Each output stream buffers incoming events before subscribtion occurs on it.
+  StreamForkDouble(Stream<T> stream) : super(stream, listenersCount: 2);
+}
+///
+/// Forks input stream on 3 output streams.
+/// Each output stream buffers incoming events before subscribtion occurs on it.
+class StreamForkTriple<T> extends StreamFork<T> {
+  /// 
+  /// Forks input [stream] on 3 output streams.
+  /// Each output stream buffers incoming events before subscribtion occurs on it.
+  StreamForkTriple(Stream<T> stream) : super(stream, listenersCount: 3);
+}
