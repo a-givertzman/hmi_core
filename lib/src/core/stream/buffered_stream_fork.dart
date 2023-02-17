63d3a60e183b9a03affe2a79cd505991970cbecb
Minyewoo <const.trushov@gmail.com>
2023-02-17 10:55:02 +0300

    Implemented stream forking

---
+++ lib/src/core/stream/buffered_stream_fork.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+
+/// Forks input stream on a given number of output streams.
+/// Each output stream buffers incoming events before any subscribtion occurs on it.
+class BufferedStreamFork<T> {
+  late final List<StreamController<T>> _controllers;
+  late final StreamSubscription<T> _subscription;
+  int _listenersCount;
+  /// 
+  /// Forks input [stream] on [listenersCount] of output streams.
+  /// Each output stream buffers incoming events before any subscribtion occurs on it.
+  BufferedStreamFork({
+    required Stream<T> stream, 
+    int listenersCount = 1,
+  }) : _listenersCount = listenersCount {
+    _controllers = List.generate(
+      listenersCount, 
+      (_) => StreamController<T>(
+        onCancel: () async {
+          if(--_listenersCount == 0) {
+            await _subscription.cancel();
+          }
+        },
+      ),
+      growable: false,
+    );
+    _subscription = stream.listen((event) {
+      for (final controller in _controllers) {
+        controller.add(event);
+      }
+    });
+  }
+  ///
+  /// Returns one of the forks of incoming stream.
+  Stream<T> stream(int index) => _controllers[index].stream;
+}
