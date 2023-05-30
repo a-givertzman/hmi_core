b97c72490242017ce0e8eaf821c2d179ca3a8b23
Minyewoo <const.trushov@gmail.com>
2023-05-30 18:44:28 +0300

    LocalStore | Set up tests

---
+++ test/unit/local_store/local_store_clear_test.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hmi_core/src/local_store/local_store.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() {
+  test('LocalStore clear removes all entries from SharedPreferences', () async {
+    final initialValues = {
+      'test1': 'value1',
+      'test2': 2,
+      'test3': true,
+    };
+    SharedPreferences.setMockInitialValues(initialValues);
+    final store = LocalStore();
+    final result = await store.clear();
+    expect(result, isTrue);
+    final preferences = await SharedPreferences.getInstance();
+    expect(preferences.getKeys(), isEmpty);
+    final repeatedResult = await store.clear();
+    expect(result, isFalse);
+  });
+}
---
+++ test/unit/local_store/local_store_remove_test.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hmi_core/src/local_store/local_store.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() {
+  test('LocalStore remove removes single entry from SharedPreferences', () async {
+    final initialValues = {
+      'test1': 'value1',
+      'test2': 2,
+      'test3': true,
+    };
+    SharedPreferences.setMockInitialValues(initialValues);
+    final store = LocalStore();
+    final result = await store.remove();
+    expect(result, isTrue);
+    final preferences = await SharedPreferences.getInstance();
+    expect(preferences.getKeys(), isEmpty);
+  });
+}
