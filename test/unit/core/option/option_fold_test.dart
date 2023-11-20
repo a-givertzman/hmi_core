4491b60dfe2b56bff7e9b5201c4c656b05d9d2f3
a-givertzman <a.givertzman@icloud.com>
2023-11-20 14:45:46 +0300

    option tests fixes

--- test/unit/core/option/option_fold_test.dart
+++ test/unit/core/option/option_fold_test.dart
@@ -1,9 +1,13 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hmi_core/hmi_core_option.dart';
 
+class ForTestOptionOnly {
+  final String value;
+  ForTestOptionOnly(this.value);
+}
 
 void main() {
-  group('Option fold', () {
+  group('Option fold strong typed', () {
     test('calls onSome function if it is instance of Some', () {
       for(int i=-50; i<=50; i++) {
         final Option<int> option = Some(i);
@@ -23,4 +27,38 @@ void main() {
       expect(foldResult, equals('None'));
     });
   });
+  group('Option fold different types', () {
+    final testData = [
+      0x7FFFFFFFFFFFFFFF,
+      -0x8000000000000000,
+      ForTestOptionOnly("value"),
+      true,
+      false,
+      double.maxFinite,
+      double.minPositive
+      -double.maxFinite,
+    ];
+    test('calls onSome function if it is instance of Some', () {
+      for(final value in testData) {
+        final Option option = Some(value);
+        final foldResult = switch(option) {
+          Some(:final value) => value,
+          None() => None,
+        };
+        expect(foldResult, equals(value));
+      }
+    });
+    test('calls onNone function if it is instance of None', () {
+      const Option option = None();
+      final foldResult = switch(option) {
+        Some(:final value) => value,
+        None() => None,
+      };
+      expect(foldResult, equals(None));
+    });
+  });
 }
+
+
+
+
