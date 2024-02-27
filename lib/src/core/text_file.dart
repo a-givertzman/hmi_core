4929a0b31b4894a3dc514015294d66c5ff189678
Minyewoo <const.trushov@gmail.com>
2024-02-27 19:08:12 +0300

    TextFile | implemented write method

--- lib/src/core/text_file.dart
+++ lib/src/core/text_file.dart
@@ -8,18 +8,30 @@ abstract class TextFile {
   const factory TextFile.asset(String assetPath) = _AssetTextFile;
   ///
   Future<String> get content;
+  ///
+  Future<void> write(String text);
 }
 ///
 class _PathTextFile implements TextFile {
   final String _filePath;
   const _PathTextFile(this._filePath);
+  //
   @override
   Future<String> get content => File(_filePath).readAsString();
+  //
+  @override
+  Future<void> write(String text) => File(_filePath).writeAsString(text, flush: true);
 }
 ///
 class _AssetTextFile implements TextFile {
   final String _assetPath;
   const _AssetTextFile(this._assetPath);
+  //
   @override
   Future<String> get content => rootBundle.loadString(_assetPath);
+  //
+  @override
+  Future<void> write(String text) {
+    throw UnimplementedError('Unable to write to asset file.');
+  }
 }
