55aa910cc58f173807e072c00a5dae426b61da3a
Minyewoo <const.trushov@gmail.com>
2024-02-29 17:16:06 +0300

    TextFile | Updated read tests

--- test/unit/core/text_file/text_file_asset_read_test.dart
+++ test/unit/core/text_file/text_file_asset_read_test.dart
@@ -6,9 +6,15 @@ void main() {
   WidgetsFlutterBinding.ensureInitialized();
   const testAssetPath = 'assets/test/test.txt';
   const originalContent = 'test\nasset\ntext';
-  test('TextFile asset reads bundle file correctly', () async {
-    const textFile = TextFile.asset(testAssetPath);
-    final receivedContent = await textFile.content;
-    expect(originalContent, equals(receivedContent));
+  group('TextFile.asset(...).read()', () {
+    test('reads existing bundle file correctly', () async {
+      const textFile = TextFile.asset(testAssetPath);
+      final receivedContent = await textFile.content;
+      expect(originalContent, equals(receivedContent));
+    });
+    test('throws error on unexisting bundle file', () async {
+      const textFile = TextFile.asset('assets/unexisting_asset.txt');
+      expect(() async => await textFile.content, throwsA(isA<Error>()));
+    });
   });
 }
--- test/unit/core/text_file/text_file_path_read_test.dart
+++ test/unit/core/text_file/text_file_path_read_test.dart
@@ -3,18 +3,41 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:hmi_core/src/core/text_file.dart';
 
 void main() {
-  const testFilePath = 'test.txt';
-  const originalContent = 'first line some test text 12345\nsecond line some test text 12345';
-  late final File testFile;
-  setUpAll(() {
-    testFile = File(testFilePath)..writeAsString(originalContent, flush: true);
-  });
-  tearDownAll(() {
-    testFile.delete();
-  });
-  test('TextFile path reads local file correctly', () async {
-    const textFile = TextFile.path(testFilePath);
-    final receivedContent = await textFile.content;
-    expect(originalContent, equals(receivedContent));
+  const testFileNamePrefix = 'text_file_path_write_test';
+  const contentToWrite = [
+    'first line some test text 12345\nsecond line some test text 12345\n',
+    '',
+    '0123456789',
+    'abcdefghijklmnopqrstuvwxyz',
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    'абвгдеёжзийклмнопрстуфхцчшщъыьэюя',
+    'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ',
+    '!@#\$%^&*()_+=-"№;:?./\\|[]{}',
+  ];
+  group('TextFile.asset(...).read()', () {
+    setUpAll(() async {
+      await Future.wait([
+        for(int i=0; i < contentToWrite.length; i++) 
+          File('$testFileNamePrefix-$i.txt').writeAsString(contentToWrite[i]),
+      ]);
+    });
+    tearDownAll(() async {
+      await Future.wait([
+        for(int i=0; i < contentToWrite.length; i++) 
+          File('$testFileNamePrefix-$i.txt').delete(),
+      ]);
+    });
+    test('reads local file correctly', () async {
+      for(int i=0; i < contentToWrite.length; i++) {
+        final fileName = '$testFileNamePrefix-$i.txt';
+        final textFile = TextFile.path(fileName);
+        final receivedContent = await textFile.content;
+        expect(receivedContent, equals(contentToWrite[i]));
+      }
+    });
+    test('throws error on unexisting file', () async {
+      const textFile = TextFile.path('unexisting_asset_file.txt');
+      expect(() async => await textFile.content, throwsA(isA<PathNotFoundException>()));
+    });
   });
 }
