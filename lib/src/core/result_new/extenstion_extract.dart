0920e56fcddc6781346d8cfe4a8e947f46dc1463
nyaneet <nyaneet@gmail.com>
2024-10-14 18:15:49 +0300

    Result | add intoOk and intoErr extensions

--- lib/src/core/result_new/extenstion_extract.dart
+++ lib/src/core/result_new/extenstion_extract.dart
@@ -1,7 +1,13 @@
 import 'package:hmi_core/hmi_core_result_new.dart';
 ///
+/// Extracting contained values
 extension Extract<V, E> on Result<V, E> {
   ///
+  /// Returns the contained [Ok] value or a provided default value [d].
+  ///
+  /// Arguments passed to [unwrapOr] are eagerly evaluated; if you are passing
+  /// the result of a function call, it is recommended to use [unwrapOrElse],
+  /// which is lazily evaluated.
   V unwrapOr(V d) {
     return switch (this) {
       Ok(:final V value) => value,
@@ -9,24 +15,37 @@ extension Extract<V, E> on Result<V, E> {
     };
   }
   ///
+  /// Returns the contained [Ok] value or computes it from a closure [d].
   V unwrapOrElse(V Function(E error) d) {
     return switch (this) {
       Ok(:final V value) => value,
       Err(:final E error) => d(error),
     };
   }
+}
+///
+/// Extracting contained values for [Ok]
+extension ExtractOk<V> on Result<V, Never> {
   ///
-  E unwrapErrOr(E d) {
-    return switch (this) {
-      Ok() => d,
-      Err(:final E error) => error,
-    };
+  /// Returns the contained [Ok] value, but never throw an error.
+  ///
+  /// It can be used as a maintainability safeguard that will fail
+  /// to compile if the error type of the [Result] is later changed
+  /// from [Never] to a type that can actually occur.
+  V intoOk() {
+    return (this as Ok).value;
   }
+}
+///
+/// Extracting contained values for [Err]
+extension ExtractErr<E> on Result<Never, E> {
   ///
-  E unwrapErrOrElse(E Function(V value) d) {
-    return switch (this) {
-      Ok(:final V value) => d(value),
-      Err(:final E error) => error,
-    };
+  /// Returns the contained [Err] value, but never throw an error.
+  ///
+  /// It can be used as a maintainability safeguard that will fail
+  /// to compile if the ok type of the [Result] is later changed
+  /// from [Never] to a type that can actually occur.
+  E intoErr() {
+    return (this as Err).error;
   }
 }
