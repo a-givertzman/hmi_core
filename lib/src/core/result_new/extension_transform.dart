d8523780e2b0efbd2f9eb0641000f555ba591709
nyaneet <nyaneet@gmail.com>
2024-10-14 18:13:30 +0300

    Result | add mapErr extension

--- lib/src/core/result_new/extension_transform.dart
+++ lib/src/core/result_new/extension_transform.dart
@@ -1,7 +1,10 @@
 import 'package:hmi_core/hmi_core_result_new.dart';
 ///
+/// Transforming contained values
 extension Transform<V, E> on Result<V, E> {
   ///
+  /// Maps a `Result<V, E>` to `Result<U, E>` by applying a function to a
+  /// contained [Ok] value, leaving an [Err] value untouched.
   Result<U, E> map<U>(U Function(V value) op) {
     return switch (this) {
       Ok(:final V value) => Ok(op(value)),
@@ -9,6 +12,8 @@ extension Transform<V, E> on Result<V, E> {
     };
   }
   ///
+  /// Maps a `Result<V, E>` to `U` by returning default value [d] (if the result is [Err]),
+  /// or applying a function [f] to the contained value (if the result is [Ok]).
   U mapOr<U>(U d, U Function(V value) f) {
     return switch (this) {
       Ok(:final V value) => f(value),
@@ -16,6 +21,8 @@ extension Transform<V, E> on Result<V, E> {
     };
   }
   ///
+  /// Maps a `Result<V, E>` to `U` by applying fallback function [d] to
+  /// a contained [Err] value, or function [f] to a contained [Ok] value.
   U mapOrElse<U>(U Function(E value) d, U Function(V value) f) {
     return switch (this) {
       Ok(:final V value) => f(value),
@@ -23,6 +30,17 @@ extension Transform<V, E> on Result<V, E> {
     };
   }
   ///
+  /// Maps a `Result<V, E>` to `Result<U, F>` by applying a function to a
+  /// contained [Err] value, leaving an [Ok] value untouched.
+  Result<V, F> mapErr<F>(F Function(E error) op) {
+    return switch (this) {
+      Ok(:final V value) => Ok(value),
+      Err(:final E error) => Err(op(error)),
+    };
+  }
+  ///
+  /// Calls a function [f] with a reference to the contained value
+  /// if the result is [Ok].
   Result<V, E> inspect(Function(V value) f) {
     if (this case Ok(:final value)) {
       f(value);
@@ -30,6 +48,8 @@ extension Transform<V, E> on Result<V, E> {
     return this;
   }
   ///
+  /// Calls a function [f] with a reference to the contained value
+  /// if the result is [Err].
   Result<V, E> inspectErr(Function(E error) f) {
     if (this case Err(:final error)) {
       f(error);
