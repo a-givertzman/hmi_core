8a34c3eeecac71a60c33ea480fee885656df61f5
Anton Lobanov <a.givertzman@icloud.com>
2024-11-07 13:57:23 +0300

    Option | extension methods | extension_transform

--- lib/hmi_core_option.dart
+++ lib/hmi_core_option.dart
@@ -2,3 +2,4 @@ library hmi_core_option;
 
 export 'src/core/option/option.dart';
 export 'src/core/option/extension_extract.dart';
+export 'src/core/option/extension_transform.dart';
---
+++ lib/src/core/option/extension_transform.dart
@@ -0,0 +1,35 @@
+import 'package:hmi_core/hmi_core_option.dart';
+import 'package:hmi_core/hmi_core_result.dart';
+
+///
+/// Transforming contained values
+extension Transform<V, E> on Option<V> {
+  ///
+  /// Transforms the `Option<T>` into a [Result<T, E>],
+  /// mapping [Some(v)] to [Ok(v)] and [None] to [Err(err)].
+  ///
+  /// Arguments passed to `ok_or` are eagerly evaluated; if you are passing the
+  /// result of a function call, it is recommended to use [`ok_or_else`], which is
+  /// lazily evaluated.
+  ///
+  /// [`Ok(v)`]: Ok
+  /// [`Err(err)`]: Err
+  /// [`Some(v)`]: Some
+  /// [`ok_or_else`]: Option::ok_or_else
+  ///
+  /// # Examples
+  ///
+  /// ```
+  /// let x = Some("foo");
+  /// assert_eq!(x.ok_or(0), Ok("foo"));
+  ///
+  /// let x: Option<&str> = None;
+  /// assert_eq!(x.ok_or(0), Err(0));
+  /// ```
+  Result<V, E> okOr(E err) {
+      return switch (this) {
+          Some(:final value) => Ok(value),
+          None() => Err(err),
+      };
+  }
+}
