7ac1bd1991bf11c398d732a2b2f5706e724949d9
Minyewoo <const.trushov@gmail.com>
2024-11-07 20:26:19 +0300

    Result | included extensions into export files

--- lib/hmi_core.dart
+++ lib/hmi_core.dart
@@ -23,6 +23,11 @@ export 'src/core/log/log_level.dart';
 export 'src/core/option/option.dart';
 // Result
 export 'src/core/result/result.dart';
+export 'src/core/result/extension_adapter.dart';
+export 'src/core/result/extension_boolean_operations.dart';
+export 'src/core/result/extension_extract.dart';
+export 'src/core/result/extension_querying.dart';
+export 'src/core/result/extension_transform.dart';
 // ValueObject
 export 'src/core/value_object/value_object.dart';
 export 'src/core/value_object/value_string.dart';
--- lib/hmi_core_result.dart
+++ lib/hmi_core_result.dart
@@ -1,3 +1,8 @@
 library hmi_core_result;
 
 export 'src/core/result/result.dart';
+export 'src/core/result/extension_adapter.dart';
+export 'src/core/result/extension_boolean_operations.dart';
+export 'src/core/result/extension_extract.dart';
+export 'src/core/result/extension_querying.dart';
+export 'src/core/result/extension_transform.dart';
