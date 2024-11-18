4c0c8e28c95b50092a4c10e2d20a8e681b9b3784
Anton Lobanov <a.givertzman@icloud.com>
2024-11-18 20:10:15 +0300

    Option | extension methods | extension_querying

--- lib/hmi_core_option.dart
+++ lib/hmi_core_option.dart
@@ -3,3 +3,4 @@ library hmi_core_option;
 export 'src/core/option/option.dart';
 export 'src/core/option/extension_extract.dart';
 export 'src/core/option/extension_transform.dart';
+export 'src/core/option/extension_querying.dart';
