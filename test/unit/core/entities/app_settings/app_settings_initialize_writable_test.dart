c6292a433afd74bd113d8a668df81d1df1e48a82
nyaneet <nyaneet@gmail.com>
2025-04-22 16:40:52 +0300

    AppSettings, Setting | add tests for cases with writable settings

---
+++ test/unit/core/entities/app_settings/app_settings_initialize_writable_test.dart
@@ -0,0 +1,234 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hmi_core/hmi_core_app_settings.dart';
+import 'package:hmi_core/src/core/json/json_map.dart';
+import 'package:hmi_core/src/core/log/log.dart';
+import 'package:hmi_core/src/core/result/result.dart';
+import 'package:hmi_core/src/core/error/failure.dart';
+import 'fake_text_file.dart';
+///
+/// Fake implementation of [JsonMap] for tests
+class FakeJsonMap implements JsonMap<dynamic> {
+  final ResultF<Map<String, dynamic>> _map;
+  ///
+  /// Creates [FakeJsonMap] with a given [map].
+  const FakeJsonMap(ResultF<Map<String, dynamic>> map) : _map = map;
+  //
+  @override
+  Future<ResultF<Map<String, dynamic>>> get decoded async {
+    return _map;
+  }
+}
+//
+void main() {
+  Log.initialize();
+  final validReadOnlyMaps = [
+    {
+      'test_setting_1': 0,
+      'test_setting_2': 27,
+      'test_setting_3': -123,
+    }, // int
+    {
+      'test_setting_1': 0.0,
+      'test_setting_2': 27.0,
+      'test_setting_3': -123.0,
+    }, // double
+    {
+      'test_setting_1': '0',
+      'test_setting_2': '27',
+      'test_setting_3': '-123',
+    }, // string
+    {
+      'test_setting_1': true,
+      'test_setting_2': false,
+      'test_setting_3': true,
+    }, // bool
+  ];
+  final validWritableMaps = [
+    {
+      'test_writable_setting_1': 0,
+      'test_writable_setting_2': 27,
+      'test_writable_setting_3': -123,
+    }, // int
+    {
+      'test_writable_setting_1': 0.0,
+      'test_writable_setting_2': 27.0,
+      'test_writable_setting_3': -123.0,
+    }, // double
+    {
+      'test_writable_setting_1': '0',
+      'test_writable_setting_2': '27',
+      'test_writable_setting_3': '-123',
+    }, // string
+    {
+      'test_writable_setting_1': true,
+      'test_writable_setting_2': false,
+      'test_writable_setting_3': true,
+    }, // bool
+  ];
+  group('AppSettings with writable initialize', () {
+    test('sets data normally with valid writable json map', () async {
+      for (final map in validWritableMaps) {
+        await AppSettings.initialize(writable: FakeJsonMap(Ok(map)));
+        expect(
+          AppSettings.getSetting('test_writable_setting_1', onError: ((err) => null)),
+          map['test_writable_setting_1'],
+        );
+        expect(
+          AppSettings.getSetting('test_writable_setting_2', onError: ((err) => null)),
+          map['test_writable_setting_2'],
+        );
+        expect(
+          AppSettings.getSetting('test_writable_setting_3', onError: ((err) => null)),
+          map['test_writable_setting_3'],
+        );
+      }
+    });
+    test('sets data normally with valid readOnly and writable json maps', () async {
+      //
+      for (final readOnly in validReadOnlyMaps) {
+        for (final writable in validWritableMaps) {
+          await AppSettings.initialize(
+            readOnly: FakeJsonMap(Ok(readOnly)),
+            writable: FakeJsonMap(Ok(writable)),
+          );
+          expect(
+            AppSettings.getSetting('test_setting_1', onError: ((err) => null)),
+            readOnly['test_setting_1'],
+          );
+          expect(
+            AppSettings.getSetting('test_setting_2', onError: ((err) => null)),
+            readOnly['test_setting_2'],
+          );
+          expect(
+            AppSettings.getSetting('test_setting_3', onError: ((err) => null)),
+            readOnly['test_setting_3'],
+          );
+           expect(
+            AppSettings.getSetting('test_writable_setting_1', onError: ((err) => null)),
+            writable['test_writable_setting_1'],
+          );
+          expect(
+            AppSettings.getSetting('test_writable_setting_2', onError: ((err) => null)),
+            writable['test_writable_setting_2'],
+          );
+          expect(
+            AppSettings.getSetting('test_writable_setting_3', onError: ((err) => null)),
+            writable['test_writable_setting_3'],
+          );
+        }
+      }
+    });
+    test('writable settings overwrite readOnly settings', () async {
+      await AppSettings.initialize(
+        readOnly: const FakeJsonMap(Ok({
+          'test_overwrite_setting_1': 0,
+          'test_overwrite_setting_2': 27,
+          'test_overwrite_setting_3': -123,
+        })),
+        writable: const FakeJsonMap(Ok({
+          'test_overwrite_setting_1': -123,
+          'test_overwrite_setting_2': 0,
+          'test_overwrite_setting_3': 27,
+        })),
+      );
+      expect(AppSettings.getSetting('test_overwrite_setting_1'), -123);
+      expect(AppSettings.getSetting('test_overwrite_setting_2'), 0);
+      expect(AppSettings.getSetting('test_overwrite_setting_3'), 27);
+    });
+    test('restore writable setting from store file', () async {
+      for (final map in validWritableMaps) {
+        final storeFile = FakeTextFile(
+          '{"test_writable_setting_1":"rewritten","test_writable_setting_2":"rewritten","test_writable_setting_3":"rewritten"}',
+        );
+        await AppSettings.initialize(
+          writable: FakeJsonMap(Ok(map)),
+          store: storeFile,
+        );
+        expect(AppSettings.getSetting('test_writable_setting_1'), "rewritten");
+        expect(AppSettings.getSetting('test_writable_setting_2'), "rewritten");
+        expect(AppSettings.getSetting('test_writable_setting_3'), "rewritten");
+      }
+    });
+    test('ignore read only settings in store file', () async {
+      for (final map in validReadOnlyMaps) {
+        final storeFile = FakeTextFile(
+          '{"test_setting_1":"rewritten","test_setting_2":"rewritten","test_setting_3":"rewritten"}',
+        );
+        await AppSettings.initialize(
+          readOnly: FakeJsonMap(Ok(map)),
+          store: storeFile,
+        );
+        expect(AppSettings.getSetting('test_setting_1'), map['test_setting_1']);
+        expect(AppSettings.getSetting('test_setting_2'), map['test_setting_2']);
+        expect(AppSettings.getSetting('test_setting_3'), map['test_setting_3']);
+      }
+    });
+    test('ignore not found settings in store file', () async {
+      final storeFile = FakeTextFile(
+        '{"test_not_found_setting_1":"rewritten","test_not_found_setting_2":"rewritten","test_not_found_setting_3":"rewritten"}',
+      );
+      await AppSettings.initialize(store: storeFile);
+      expect(
+        AppSettings.getSetting('test_not_found_setting_1', onError: ((err) => null)),
+        null,
+      );
+      expect(
+        AppSettings.getSetting('test_not_found_setting_2', onError: ((err) => null)),
+        null,
+      );
+      expect(
+        AppSettings.getSetting('test_not_found_setting_3', onError: ((err) => null)),
+        null,
+      );
+    });
+    test('getSetting onError passes new value case error', () async {
+      final notFoundMaps = [
+        {
+          'key': 'not_found_1',
+          'map_value': 0,
+        }, // int
+        {
+          'key': 'not_found_2',
+          'map_value': 0.0,
+        }, // int
+        {
+          'key': 'not_found_3',
+          'map_value': false,
+        }, // bool
+        {
+          'key': 'not_found_4',
+          'map_value': '0',
+        }, // string
+        {
+          'key': 'not_found_5',
+          'map_value': null,
+        }, // null
+      ];
+      for (final map in notFoundMaps) {
+        await AppSettings.initialize();
+        final key = map['key'] as String;
+        final mapValue = map['map_value'] as dynamic;
+        expect(
+          AppSettings.getSetting(key, onError: ((err) => mapValue)),
+          mapValue,
+        );
+      }
+    });
+    test('does not throw error with invalid json map', () async {
+      final mapWithError = FakeJsonMap(
+        Err(Failure(
+          message: 'invalid json map',
+          stackTrace: StackTrace.current,
+        )),
+      );
+      await expectLater(
+        AppSettings.initialize(writable: mapWithError),
+        completes,
+      );
+      await expectLater(
+        AppSettings.initialize(readOnly: mapWithError, writable: mapWithError),
+        completes,
+      );
+    });
+  });
+}
